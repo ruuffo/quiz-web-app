cb3472259bbbbca1f94b32f54d0382cd2191a5e1
lukii28 <ambre.casanova@yahoo.fr>
2022-12-16 16:43:47 +0100

    UI implement first front vers

--- quiz-ui/src/router/index.js
+++ quiz-ui/src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomePage from '../views/Homepage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "HomePage",
+      component: HomePage
     },
     {
       path: '/about',
---
+++ quiz-ui/src/views/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Home page</h1>
+</template>
+
+<script>
+import quizApiService from "@/services/QuizApiService";
+
+export default {
+  name: "HomePage",
+  data() {
+    return {
+      registeredScores: []
+    };
+  },
+  async created() {
+    console.log("Composant Home page 'created'");
+  }
+};
+</script>
