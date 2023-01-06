bb38dcaa2de4926abf3e221f4b0adcd37728dcde
fogelr <ruben.fogel@edu.esiee.fr>
2023-01-06 16:54:05 +0100

    ajout du login dans  "add question"

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -26,25 +26,44 @@ def GetQuizInfo():
 def postLogin():
     payload = request.get_json()
     if (payload["password"] == 'flask2023'):
-        return jwt_utils.build_token()
+        return {'token': jwt_utils.build_token()}
     else:
         return 'Unauthorized', 401
 
 
 @app.route('/questions', methods=['POST'])
 def add_question():
+
+
+    auth_header = request.headers.get('Authorization')
+    if auth_header is None:
+        return 'Unauthorized', 401
+
+    auth_type, auth_token = auth_header.split()
+    if auth_type != 'Bearer':
+        return 'Unauthorized', 401
+
+    try:
+        jwt_utils.decode_token(auth_token)
+    except jwt_utils.TokenError:
+        return 'Unauthorized', 401
+
+    # If the client is authenticated, proceed with adding the question to the database
     return db_utils.add_question()
 
 
-@app.route('/questions/1', methods=['DEL'])
-def delete_question():
-    return db_utils.delete_question()
+@app.route('/questions/<int:id>', methods=['DELETE'])
+def delete_question(id : int):
+    return db_utils.delete_question(id)
 
 
-@app.route('/questions/all', methods=['DEL'])
+@app.route('/questions/all', methods=['DELETE'])
 def delete_all_questions():
     return db_utils.delete_all_questions()
 
+@app.route('/rebuild-db', methods=['POST'])
+def rebuild_db():
+    return db_utils.rebuild_db()
 
 if __name__ == "__main__":
     app.run()
